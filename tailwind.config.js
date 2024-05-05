6030e37faa2053f3f0f74941a6935d868731a25f
Roshanshrimali <167690840+Roshanshrimali@users.noreply.github.com>
2024-05-05 22:16:17 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {},
+    // screens: {
+    //   'mobile': '200px',
+    //   // => @media (min-width: 640px) { ... }
+
+    //   'tablet': '500px',
+    //   // => @media (min-width: 1024px) { ... }
+
+    //   'desktop': '1025px',
+    //   // => @media (min-width: 1280px) { ... }
+    // },
+  },
+  plugins: [],
+}
+
